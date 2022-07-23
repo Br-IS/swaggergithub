1ee8e8c8b54903225737dbbd680da6121cde609f
Br-IS <boris.quizhpe.est@tecazuay.edu.ec>
2022-07-23 11:37:22 -0500

    cors

--- src/main/java/edu/ista/m5b/M5bApplication.java
+++ src/main/java/edu/ista/m5b/M5bApplication.java
@@ -2,12 +2,24 @@ package edu.ista.m5b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class M5bApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(M5bApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry corsRegistry){
+				corsRegistry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
 	}
 
 }
